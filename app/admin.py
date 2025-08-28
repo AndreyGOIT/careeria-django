42585e6e56f9ec3c2b236a44c0e05e1809542a4e
AndreyGOIT <ae.borga@gmail.com>
2025-08-28 14:45:48 +0300

    superuser luominen

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
+# Tiedosto: suppliers/app/admin.py
+# Admin käyttöliittymän määrittelyt.
+# Admin käyttöliittymä on Django:n sisäänrakennettu työkalu,
+# jolla voi hallinnoida sovelluksen tietoja web-käyttöliittymässä.
+# Admin käyttöliittymä on hyödyllinen työkalu kehityksen aikana
+# ja pienimuotoisissa projekteissa, joissa ei tarvita erillistä
+# hallintapaneelia.
+
+# Jos rekisteröi tällä tavalla adminille oman appin
+# modelit, voi myös admin sivuilta muokata näitä tietoja.
+
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Supplier, Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    pass
